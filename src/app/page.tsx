00e3810e279b30a0e354021341b2303412f56208
Nachsyas Arham Mumtaz Nashohi <nchsys.arham@gmail.com>
2026-05-12 18:26:36 +0700

    feat: integrate ingredient marquee and mascot into landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,30 @@ import { USPSection } from "@/components/shared/USPSection";
 import { AboutTeam } from "@/components/shared/AboutTeam";
 import { Footer } from "@/components/shared/Footer";
 import { ProductList } from "@/components/shared/ProductList";
+import { IngredientWalking } from "@/components/shared/IngredientWalking";
+import { Mascot } from "@/components/shared/Mascot";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col">
+    <main className="min-h-screen flex flex-col relative overflow-hidden">
       <Navbar />
-      
+
       <div className="flex-1">
         <HeroSection />
         <USPSection />
-        
+
+        {/* ANIMASI IKON BERJALAN MUNCUL DI SINI (Di atas Katalog) */}
+        <IngredientWalking />
+
         <ProductList />
 
         <AboutTeam />
       </div>
 
       <Footer />
+
+      {/* MASKOT KITA NONGKRONG DI SINI (Pojok Layar) */}
+      <Mascot />
     </main>
   );
 }
